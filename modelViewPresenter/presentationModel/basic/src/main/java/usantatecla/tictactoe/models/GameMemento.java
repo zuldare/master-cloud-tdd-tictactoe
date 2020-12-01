0616b70663019cdf64b15f00b312f4b6ace6e719
jaime.hernandez <jaime.hernandez@bi-geek.com>
2020-12-01 22:42:59 +0100

    [VERDE] Añado constructor y getters para pasar los tests

--- modelViewPresenter/presentationModel/basic/src/main/java/usantatecla/tictactoe/models/GameMemento.java
+++ modelViewPresenter/presentationModel/basic/src/main/java/usantatecla/tictactoe/models/GameMemento.java
@@ -2,4 +2,19 @@ package usantatecla.tictactoe.models;
 
 public class GameMemento {
 
+    private Board board;
+    private Turn turn;
+
+    public GameMemento(Board board, Turn turn){
+        this.board = board;
+        this.turn =  turn;
+    }
+
+    public Board getBoard() {
+        return board;
+    }
+
+    public Turn getTurn() {
+        return turn;
+    }
 }
--- modelViewPresenter/presentationModel/basic/src/test/java/usantatecla/tictactoe/models/GameMementoTest.java
+++ modelViewPresenter/presentationModel/basic/src/test/java/usantatecla/tictactoe/models/GameMementoTest.java
@@ -1,18 +1,34 @@
 package usantatecla.tictactoe.models;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.mock;
 
 public class GameMementoTest {
 
     private GameMemento gameMemento;
 
+    @Mock
+    private Board board;
+
+    @Mock
+    private Turn turn;
+
+    @BeforeEach
+    private void beforeTest(){
+        this.board = mock(Board.class);
+        this.turn = mock(Turn.class);
+    }
+
     @Test
     public void testGameMemento(){
-        this.gameMemento = new GameMemento();
+        this.gameMemento = new GameMemento(board, turn);
         assertNotNull(this.gameMemento);
         assertNotNull(this.gameMemento.getBoard());
         assertNotNull(this.gameMemento.getTurn());
