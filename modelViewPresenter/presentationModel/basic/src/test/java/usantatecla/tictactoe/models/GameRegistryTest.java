f91f78ecc0510d8642e9050d79934248c19e4c9f
jaime.hernandez <jaime.hernandez@bi-geek.com>
2020-12-01 22:45:18 +0100

    [ROJO] No existe la clase registry para el patron memento.

---
+++ modelViewPresenter/presentationModel/basic/src/test/java/usantatecla/tictactoe/models/GameRegistryTest.java
@@ -0,0 +1,13 @@
+package usantatecla.tictactoe.models;
+
+import org.junit.jupiter.api.Test;
+
+public class GameRegistryTest {
+
+    private GameRegistry gameRegistry;
+
+    @Test
+    public void testGameRegistry(){
+
+    }
+}
